50290431724c6f42cbf94add0439fe2187550a61
Jay Tabudlo <jaytabudlo@hotmail.com>
2021-06-16 17:15:14 +0200

    other single test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -28,29 +28,29 @@ class LoginTest extends DuskTestCase
         ]);
     }
 
-    public function testLoginMissingPassword()
-    {
-      $this->browse(function (Browser $browser) {
-        $browser->visit(new Login)
-                ->type('@email', 'johndoe@example.com')
-                ->type('@password', ' ')
-                ->press('Login')
-                ->waitForText('The password field is required')
-                ->assertSee('The password field is required');
-      });
-    }
-
-    // public function testLoginIncorrectPassword()
+    // public function testLoginMissingPassword()
     // {
     //   $this->browse(function (Browser $browser) {
     //     $browser->visit(new Login)
     //             ->type('@email', 'johndoe@example.com')
-    //             ->type('@password', 'incorrect')
+    //             ->type('@password', ' ')
     //             ->press('Login')
-    //             ->waitForText('These credentials do not match our records.')
-    //             ->assertSee('These credentials do not match our records.');
+    //             ->waitForText('The password field is required')
+    //             ->assertSee('The password field is required');
     //   });
     // }
+
+    public function testLoginIncorrectPassword()
+    {
+      $this->browse(function (Browser $browser) {
+        $browser->visit(new Login)
+                ->type('@email', 'johndoe@example.com')
+                ->type('@password', 'incorrect')
+                ->press('Login')
+                ->waitForText('These credentials do not match our records.')
+                ->assertSee('These credentials do not match our records.');
+      });
+    }
 
     // public function testLoginCorrectPassword()
     // {
