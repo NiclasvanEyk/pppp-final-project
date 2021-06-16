f627c8b31f074758d8682be5160cd11e0dad3973
Jay Tabudlo <jaytabudlo@hotmail.com>
2021-06-16 17:12:33 +0200

    Single test test

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -7,9 +7,27 @@ use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Tests\Browser\Pages\Login;
 use Tests\Browser\Pages\Dashboard;
+use App\Models\User;
+use App\Models\Account;
 
 class LoginTest extends DuskTestCase
 {
+
+    use DatabaseMigrations;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'account_id' => Account::create(['name' => 'Acme Corporation'])->id,
+            'first_name' => 'John',
+            'last_name' => 'Doe',
+            'email' => 'johndoe@example.com',
+            'owner' => true,
+        ]);
+    }
+
     public function testLoginMissingPassword()
     {
       $this->browse(function (Browser $browser) {
@@ -22,28 +40,28 @@ class LoginTest extends DuskTestCase
       });
     }
 
-    public function testLoginIncorrectPassword()
-    {
-      $this->browse(function (Browser $browser) {
-        $browser->visit(new Login)
-                ->type('@email', 'johndoe@example.com')
-                ->type('@password', 'incorrect')
-                ->press('Login')
-                ->waitForText('These credentials do not match our records.')
-                ->assertSee('These credentials do not match our records.');
-      });
-    }
+    // public function testLoginIncorrectPassword()
+    // {
+    //   $this->browse(function (Browser $browser) {
+    //     $browser->visit(new Login)
+    //             ->type('@email', 'johndoe@example.com')
+    //             ->type('@password', 'incorrect')
+    //             ->press('Login')
+    //             ->waitForText('These credentials do not match our records.')
+    //             ->assertSee('These credentials do not match our records.');
+    //   });
+    // }
 
-    public function testLoginCorrectPassword()
-    {
-      $this->browse(function (Browser $browser) {
-        $browser->visit(new Login)
-                ->type('@email', 'johndoe@example.com')
-                ->type('@password', 'secret')
-                ->press('Login')
-                ->waitForText('Dashboard')
-                ->on(new Dashboard);
-      });
-    }
+    // public function testLoginCorrectPassword()
+    // {
+    //   $this->browse(function (Browser $browser) {
+    //     $browser->visit(new Login)
+    //             ->type('@email', 'johndoe@example.com')
+    //             ->type('@password', 'secret')
+    //             ->press('Login')
+    //             ->waitForText('Dashboard')
+    //             ->on(new Dashboard);
+    //   });
+    // }
 }
 
